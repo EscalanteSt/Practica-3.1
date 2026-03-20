31c44aa49ede07a9bdeaa31d9d5ea7a8a9ead94d
EscalanteSt <see290907@gmail.com>
2026-03-20 10:40:17 -0900

    Main.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        AutoMovil auto = new AutoMovil();
+        auto.setMarca("Toyota");
+        auto.setModelo("Corolla");
+        auto.setAño(2023);
+        auto.setColor("Blanco");
+
+        System.out.println("Marca: " + auto.getMarca());
+        auto.encender();
+        auto.acelerar();
+
+        System.out.println("\n--- Honda Civic ---");
+        
+        HondaCivic honda = new HondaCivic();
+        honda.setMarca("Honda");
+        honda.setModelo("Civic");
+        honda.setAño(2024);
+        honda.setColor("Rojo");
+
+        System.out.println("Marca: " + honda.getMarca());
+        honda.encender();
+        honda.acelerar();
+    }
+}
